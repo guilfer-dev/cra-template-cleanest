52601ee2e44baecb1d4ee5ef5fc63b14fd72ab03
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2021-10-30 00:54:49 -0300

    [final] create react template

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# cra-template-cleanest-mod
+# cra-template-cleanest
  
+ A template with almost the bare minimum to create a react app.
+
+ ## Importante to notice:
+  - StrictMode is kept, but any testing tools must added.
+  - I setted up a reset css file, but it mighty not be good for you
+  - The file robot.txt now blocks the content, but can disturb your testing
+  - README file is empty
+  - index.html and manifest.json files don't have most of its metadata
+
+**Your new installed react app will look like this:**
+
+<p align="center">
+  <img src=./template-tree.png>
+</p>
--- template/src/index.js
+++ template/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <p>Hello World!</p>
+    <p>Don't forget to add your metadata on index.html and manifest.json</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
